9274ff95c97ca101cf078672c9f7124e7c99b1fa
mykonwest <ben.dlmp@gmail.com>
2023-08-14 12:07:12 -0300

    Created project with Spring Initializr

---
+++ backend/src/main/java/org/pmp/ProjMgntPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.pmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjMgntPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjMgntPlatformApplication.class, args);
+	}
+
+}
